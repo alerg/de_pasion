88819f16b7462ef102bd5430a189f0121cad9160
AleG <alejandroraulgarcia@gmail.com>
2016-04-13 14:32:06 -0300

    Puerto

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-app.listen(80, function () {
+app.listen(8080, function () {
 	console.log(__dirname);
 });
