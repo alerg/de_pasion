f0931fedf13225b9d512509bbc455b826c535fe6
AleG <alejandroraulgarcia@gmail.com>
2016-04-16 12:13:57 -0300

    'Reformas'

---
+++ public/scripts/experiment.js
@@ -0,0 +1,24 @@
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    var getExperiment = function() {
+        return cxApi.chooseVariation();
+    }
+    
+    try{
+        window.experiment = getExperiment() || 1;
+    }catch(err){
+         window.experiment = 1    
+    }
+    
+    console.log("Experiment: " + window.experiment);
+
+    ga('create', 'UA-76336068-1', 'auto');
+    ga('send', 'pageview');
---
+++ public/scripts/index.js
@@ -0,0 +1,12 @@
+document.getElementById('experiment'+ window.experiment).classList.add('show');
+document.getElementById('experiment'+ window.experiment).children[0].classList.add('show');
+var preguntaActual = 1;
+
+function continuar(){
+	switch(window.experiment){
+		case 1:
+			document.getElementById('experiment'+ window.experiment).children[0].classList.remove('show');
+			document.getElementById('experiment'+ window.experiment).children[1].classList.add('show');
+		break;
+	}
+}
